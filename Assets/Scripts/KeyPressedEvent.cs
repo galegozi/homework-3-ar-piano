0be0b179af5aa932e9566c084efaaf898a8b55da
Gal Egozi <geegozi@gmail.com>
2021-04-01 22:58:03 -0500

    Adding the panel with the key name

---
+++ Assets/Scripts/KeyPressedEvent.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KeyPressedEvent : MonoBehaviour
+{
+    public Text keyName;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Pressed(GameObject p)
+    {
+        keyName.text = p.name;
+    }
+
+    public void Released()
+    {
+        keyName.text = string.Empty;
+    }
+}
